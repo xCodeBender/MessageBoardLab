4ed8dbd1e22e49755ae7025de430cf5c39fb2b86
Alice Ogadinma <aliceamarawell@gmail.com>
2021-06-02 20:04:15 -0700

    First commit

---
+++ MessageBoardLab/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using MessageBoardLab.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBoardLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Message()
+        {
+            List<Messages> messageList = new List<Messages>();
+            using(MessageDBContext context = new MessageDBContext())
+            {
+                messageList = context.Messages.ToList();
+            }
+            return View(messageList);
+        }
+
+        [Authorize, HttpPost]
+        public IActionResult Message(string message)
+        {
+            using (MessageDBContext context = new MessageDBContext())
+            {
+                Messages newMessage = new Messages();
+                newMessage.UserId = User.Identity.Name;
+                newMessage.PostedTime = DateTime.Now;
+                newMessage.Updated = false;
+                newMessage.Message = message;
+                context.Add(newMessage);
+                context.SaveChanges();
+
+            }
+            return Redirect("Message");
+        }
+
+        
+
+        [Authorize]
+        public IActionResult Edit(int id)
+        {
+
+            using (MessageDBContext context = new MessageDBContext())
+            {
+                Messages newM = new Messages();
+                newM = context.Messages.ToList().Find(o => o.Id == id);
+
+                newM.Updated = false;
+                return View(newM);
+
+            }
+        }
+
+        [Authorize, HttpPost]
+        public IActionResult Edits(string messages, int id)
+        {
+            Messages newM= new Messages();
+            using (MessageDBContext context = new MessageDBContext())
+            {
+                newM = context.Messages.ToList().Find(o => o.Id == id);
+               
+                newM.Updated = true;
+                newM.Message = messages;
+                
+                context.SaveChanges();
+
+            }
+            return RedirectToAction("Message");
+        }
+
+        [Authorize, HttpPost, HttpDelete]
+        public IActionResult Deleted(int id, string message)
+        {
+            Messages newM = new Messages();
+            using (MessageDBContext context = new MessageDBContext())
+            {
+                newM = context.Messages.ToList().Find(o => o.Id == id);
+
+                newM.Message = message;
+                context.Messages.Remove(newM);
+                context.SaveChanges();
+
+            }
+            return RedirectToAction("Message");
+        }
+
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MessageBoardLab/Models/MessageDBContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace MessageBoardLab
+{
+    public partial class MessageDBContext : DbContext
+    {
+        public MessageDBContext()
+        {
+        }
+
+        public MessageDBContext(DbContextOptions<MessageDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Messages> Messages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=MessageDB; Integrated Security=SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Messages>(entity =>
+            {
+                entity.Property(e => e.Message).HasMaxLength(200);
+
+                entity.Property(e => e.PostedTime).HasColumnType("datetime");
+
+                entity.Property(e => e.UserId).HasMaxLength(200);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
